34f6b39f88967f7333e3ecf9d3b5a2e38015b7e6
Abdu1RH <abdul.rh2403@gmail.com>
2023-02-27 11:30:39 +0100

    THIRD AND FINAL COMMIT

--- Android Studio & Hello World/app/src/main/java/com/example/androidstudiohelloworld/MainActivity.java
+++ Android Studio & Hello World/app/src/main/java/com/example/androidstudiohelloworld/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.androidstudiohelloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    //String TAG = "MainActivity";
+    String TAG = this.getClass().getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("hello");
+        //Log.d("CREATE TAG", "onCreate: Activity has now been created");
+        Log.d(TAG, "onCreate: App up and running ");
     }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: Pausing the app");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: Now resuming the application");
+    }
+
+
+
 }
