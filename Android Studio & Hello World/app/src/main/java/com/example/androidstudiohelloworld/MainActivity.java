2d1851c3736a8d32b0c382ced0ff98e9f716b644
Abdu1RH <abdul.rh2403@gmail.com>
2023-02-27 11:32:37 +0100

    THIRD AND FINAL COMMIT

--- Android Studio & Hello World/app/src/main/java/com/example/androidstudiohelloworld/MainActivity.java
+++ Android Studio & Hello World/app/src/main/java/com/example/androidstudiohelloworld/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("hello");
-        //Log.d("CREATE TAG", "onCreate: Activity has now been created");
         Log.d(TAG, "onCreate: App up and running ");
     }
     @Override
